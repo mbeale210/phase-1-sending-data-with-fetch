57718087f9a809dd3f4b9d160da506c8106e999e
Mark Beale <bealebeaverst@gmail.com>
2024-07-25 14:49:27 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      document.body.innerHTML += data.id;
+      return data.id;
+    })
+    .catch((error) => {
+      document.body.innerHTML += error.message;
+      throw error;
+    });
+}
 // Add your code here
